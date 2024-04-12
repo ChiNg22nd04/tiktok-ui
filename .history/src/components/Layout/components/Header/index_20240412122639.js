3979c141d5d2bb742b95f555bc5d730da748dfc6
ChiNg22nd04 <dedonai99@gmail.com>
2024-04-12 14:02:55 +0700

    Xây dựng UI phần Header #6: userCurrent

---
+++ .history/src/components/Layout/components/Header/index_20240412122639.js
@@ -0,0 +1,130 @@
+import { useEffect, useState } from 'react';
+
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faCircleXmark,
+    faSpinner,
+    faMagnifyingGlass,
+    faAdd,
+    faEllipsisVertical,
+    faLanguage,
+    faKeyboard,
+    faQuestionCircle,
+    faLightbulb,
+} from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
+
+import images from '~/assets/images';
+import styles from './Header.module.scss';
+import 'tippy.js/dist/tippy.css'; // optional
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItem';
+import Button from '~/components/Button';
+import Menu from '~/components/Popper/Menu';
+
+const cx = classNames.bind(styles);
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faLightbulb} />,
+        title: 'LIVE Creator Hub',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faLanguage} />,
+        title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'English',
+                },
+                {
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Tiếng việt',
+                },
+            ],
+        },
+    },
+    {
+        icon: <FontAwesomeIcon icon={faQuestionCircle} />,
+        title: 'Feedback and helps',
+        to: '/feedback',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcuts',
+    },
+];
+
+function Header() {
+    const [searchResult, setSearchResult] = useState([]);
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([]);
+        }, 0);
+    }, []);
+
+    // handle logic
+    const handleMenuChange = (menuItem) => {
+        console.log(menuItem);
+    };
+
+    console.log(cx('wrapper'));
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('content')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Logo tiktok" />
+                </div>
+
+                <Tippy
+                    interactive
+                    visible={searchResult.length > 0}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h4 className={cx('search-title')}>Accounts</h4>
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                            </PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder="Search" spellCheck="false" />
+                        <button className={cx('clear-btn')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
+
+                {}
+
+                <div className={cx('action')}>
+                    <Button text outline className={cx('custom-uploadBtn')} leftIcon={<FontAwesomeIcon icon={faAdd} />}>
+                        Upload
+                    </Button>
+                    <Button primary>Log in</Button>
+
+                    <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Menu>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ .history/src/components/Popper/Menu/MenuItem_20240412134916.js
@@ -0,0 +1,16 @@
+import Button from '~/components/Button';
+import styles from './Menu.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+const classes = cx('menu-item');
+
+function MenuItem({ data, onClick }) {
+    return (
+        <Button className={cx('menu-item')} to={data.to} leftIcon={data.icon } onClick={onClick}>
+            {data.title}
+        </Button>
+    );
+}
+
+export default MenuItem;
